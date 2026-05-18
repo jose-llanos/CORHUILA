8a4c88f604467c800432d0a2cbaa89474a8ff044
seamyong17 <seamyongagudeloo@gmail.com>
2026-05-18 03:18:25 -0500

    Delete 2026A/calidad-software/talleres/tercer-corte/src directory

--- 2026A/calidad-software/talleres/tercer-corte/src/app/producto/producto.ts
+++
@@ -1,9 +0,0 @@
-// producto.ts
-export class Producto {
-    id: number;
-    nombre: string;
-    descripcion: string;
-    precio: number;
-    cantidad: number;
-    imagenUrl: string;  // Para la imagen del producto
-  }
--- 2026A/calidad-software/talleres/tercer-corte/src/main/java/com/panaderia/panaderia/repository/IProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.panaderia.panaderia.repository;
-
-
-
-import com.panaderia.panaderia.models.Producto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IProductoRepository extends CrudRepository<Producto, Long> {
-}
