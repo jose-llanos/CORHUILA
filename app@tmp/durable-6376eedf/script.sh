3e51f779f76b7058719f994d41895a39ad15a018
JulianaFerro1428 <mjferro-2023a@corhuila.edu.co>
2026-05-17 19:28:09 -0500

    Arreglos

--- app@tmp/durable-6376eedf/script.sh
+++
@@ -1,3 +0,0 @@
-
-                            mvn sonar:sonar                               -Dsonar.projectKey=autospark                               -Dsonar.projectName=autospark                               -Dsonar.host.url=$SONAR_HOST_URL                               -Dsonar.token=$SONAR_TOKEN
-                        
